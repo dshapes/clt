4bbf9202ffa5bb894300df81ca2ce2c428342c4a
Nishit <nishit@Nishits-MacBook-Air.local>
2020-12-20 22:31:16 +0530

    initial commit

--- app/src/main/java/com/techmate/woocommerce/adapter/CartItemAdapter.java
+++ app/src/main/java/com/techmate/woocommerce/adapter/CartItemAdapter.java
@@ -1,4 +1,55 @@
 package com.techmate.woocommerce.adapter;
 
-public class CartItemAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.techmate.woocommerce.R;
+
+public class CartItemAdapter extends RecyclerView.Adapter<CartItemAdapter.ViewHolder> {
+
+    private static final String TAG = "CartItemAdapter";
+    private Context context;
+
+    public CartItemAdapter(Context context){
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CartItemAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.raw_cart_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CartItemAdapter.ViewHolder holder, int position) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return 20;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView imgCartItem, imgMoreoOptions;
+        private TextView txtTitle, txtColor, txtSize, txtQty, txtPrice;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imgCartItem = itemView.findViewById(R.id.imgCartItem);
+            imgMoreoOptions = itemView.findViewById(R.id.imgMoreoOptions);
+            txtTitle = itemView.findViewById(R.id.txtTitle);
+            txtColor = itemView.findViewById(R.id.txtColor);
+            txtSize = itemView.findViewById(R.id.txtSize);
+            txtQty = itemView.findViewById(R.id.txtQty);
+            txtPrice = itemView.findViewById(R.id.txtPrice);
+
+        }
+    }
 }
--- app/src/main/java/com/techmate/woocommerce/ui/CartActivity.java
+++ app/src/main/java/com/techmate/woocommerce/ui/CartActivity.java
@@ -1,14 +1,49 @@
 package com.techmate.woocommerce.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.View;
+import com.techmate.woocommerce.R;
+import com.techmate.woocommerce.adapter.CartItemAdapter;
+import com.techmate.woocommerce.databinding.ActivityCartBinding;
 
-public class CartActivity extends AppCompatActivity {
+public class CartActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityCartBinding binding;
+    private Context context;
+    private CartItemAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart);
+        context = this;
+        binding = ActivityCartBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.imgLogo.setOnClickListener(this);
+        binding.recyclerCart.setLayoutManager(new LinearLayoutManager(context));
+        binding.recyclerCart.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+                outRect.set((int) getResources().getDimension(R.dimen._10sdp),10,(int) getResources().getDimension(R.dimen._10sdp),10);
+            }
+        });
+        adapter = new CartItemAdapter(context);
+        binding.recyclerCart.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imgLogo:
+                onBackPressed();
+                break;
+        }
     }
 }
